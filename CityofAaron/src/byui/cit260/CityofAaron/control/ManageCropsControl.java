d7c75ee27c84c5f0958062d956520490a42075af
Kemblesque <beau.c.kemble@gmail.com>
2018-06-04 16:55:39 -0600

    Adding sellLand and eatenByRats to manageCropsControl

--- CityofAaron/src/byui/cit260/CityofAaron/control/ManageCropsControl.java
+++ CityofAaron/src/byui/cit260/CityofAaron/control/ManageCropsControl.java
@@ -48,6 +48,51 @@ public class ManageCropsControl {
         return wheatHarvested;
     }
 
+    
+/* Start Beau Kembles Individual assignment. 
+* calculateSellLand 
+* if acresToSell < 0 return error message
+* if acresOwned < acresToSell return error message
+* if price > wheatStored return error message
+* newWheat = acresToSell * randomGeneratedNumber
+* setAcresOwned = getAcresOwned - acresToSell
+* setWheatInStorrage = getWheatInStorage + newWheat
+*/
+
+   
+    public static int calculateLandPrice(){    
+    //calculate a random price of land between 17 and 27
+    //return: price of land 
+    int landPrice = getRandomNumberInRange(17,27);
+    return landPrice;
+    }
+    
+    public static int sellLand  (int landPrice, int acresToSell, Game game){
+        
+     //if acresToSell < 0 return -1
+     if (acresToSell < 0)
+        return -1;
+    
+     //if acresToSell > acresOwned, return -1
+        int acresOwned = game.getAcresOwned();
+        if (acresToSell > acresOwned)
+            return -1;
+        
+     //acresOwned = acresOwned - acresToSell
+        acresOwned = acresOwned - acresToSell;
+        game.setAcresOwned(acresOwned); 
+        
+     //wheatStorage = wheatStorage + acresToSell * landPrice
+        int wheatStorage = game.getWheatStorage();
+        wheatStorage = wheatStorage + (acresToSell * landPrice);
+        game.setWheatStorage(wheatStorage);    
+
+    //return acresOwned    
+        return acresOwned;
+    
+    }
+
+    
 /*  Start James Jennings individual assignment.
 *calculateLandPlanted (numAcres: int, wheatStored: int, landOwned: int) returns an int
 *if numAcres is less than 0 then return -1
@@ -71,4 +116,52 @@ public class ManageCropsControl {
         }
         else return numAcres / 2;
     }
+
+/* Beau Kembles Individual assignment. 
+* calculateWheatEatenByRats
+* Generate random number between 1 & 100
+* If random number < 30 some wheat will be eaten by rats
+* If T&O 8-12% random value between 3-7% of wheatStorage eaten by rats 
+* If T&O < 8% random value between 6-10% of wheatStorage eaten by rats
+* If T&O > 12% random value between 3-5% of wheatStorage eaten by rats 
+* Subtract wheat eaten by rats from wheatStorage
+* Save updated amount of wheat in wheatStorage
+*/
+
+    private static double getRandomNumberInRange(double min, double max) {
+		//Random r = new Random();
+		//return r.nextInt((max - min) + 1) + min;
+                return randomGenerator.nextInt((int) ((max - min) + 0x1)) + min;
+    }
+public double calculateWheatEatenByRats(double tithing, int eatenByRats, Game game){    
+    //calculate a random number between 1 and 100
+    //if less than 30 some wheat will be eaten by rats
+    int wheatStorage = game.getWheatStorage();
+    int ratsChance = getRandomNumberInRange(1,100);
+    if (ratsChance < 30){
+        //If T&O 8-12% random value between 3-7% of wheatStorage eaten by rats 
+        if (tithing > 0.07 && tithing < 0.12) {
+            eatenByRats = (int) getRandomNumberInRange(0.03,0.07);
+            }
+        //If T&O < 8% random value between 6-10% of wheatStorage eaten by rats
+        if (tithing < 0.08) {
+            eatenByRats = (int) getRandomNumberInRange(0.06,0.1);
+        }
+        //If T&O > 12% random value between 3-5% of wheatStorage eaten by rats 
+        if (tithing > 0.12) {
+            eatenByRats = (int) getRandomNumberInRange(0.03,0.05);
+        }
+        //if random number > 30
+        else{
+          eatenByRats = 0;
+        }
+    }
+        
+        wheatStorage = wheatStorage - (eatenByRats * wheatStorage);
+        return wheatStorage;
+    
+    
+}
+
+
 }
