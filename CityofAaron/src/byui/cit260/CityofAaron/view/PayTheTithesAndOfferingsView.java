99a58e835da5b4528213e15375a32eb8a4d0769b
jljennings <38636661+jljennings@users.noreply.github.com>
2018-06-13 22:33:46 -0600

    Lesson 8 individual assignment

---
+++ CityofAaron/src/byui/cit260/CityofAaron/view/PayTheTithesAndOfferingsView.java
@@ -0,0 +1,143 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.CityofAaron.view;
+import byui.cit260.CityofAaron.control.ManageCropsControl;
+import java.util.Scanner;
+/**
+ *
+ * @author jennings
+ */
+public class PayTheTithesAndOfferingsView {
+    
+     /**
+     * The message that will be displayed by this view.
+     */
+    protected String message;
+    
+    /**
+     * Constructor
+     */
+    public PayTheTithesAndOfferingsView(){
+        
+        message = "The commandment of God is to pay 10% of your income each yaer\n"
+                + "Youe may choose to what extent you follow Gods law.\n"
+                + "Please decide what percentage you want to pay from 0-100.\n"
+                + "The scriptures say when you pay an honest tith the Lord will\n"
+                + "open the windows of heaven and pore out blessing upon you.";
+                
+    }
+    
+    
+    /**
+     * Get the user's input. Keep prompting them until they enter a value.
+     * @param prompt
+     * @param allowEmpty - determine whether the user can enter no value (just a return key)
+     * @return 
+     */
+    protected String getUserInput(String prompt, boolean allowEmpty){
+        
+        Scanner keyboard = new Scanner(System.in);
+        String input = "";
+        boolean inputReceived = false;
+        
+        while(inputReceived == false){
+            
+            System.out.println(prompt);
+            input = keyboard.nextLine();
+            
+            // Make sure we avoid a null-pointer error.
+            if (input == null){
+                input = "";
+            }
+            
+            // Trim any trailing whitespace, including the carriage return.
+            input = input.trim();
+            
+            if (input.equals("") == false || allowEmpty == true){
+                inputReceived = true;
+            }
+        }
+        
+        return input;
+    }
+    
+    
+    /**
+     * An overloaded version of getUserInput that sets allowEmpty to false so we don't have 
+     * to type it ourselves.
+     * @param prompt
+     * @return 
+     */
+    protected String getUserInput(String prompt){
+        return getUserInput(prompt, false);
+    }
+    
+    /**
+     * Get the set of inputs from the user.
+     * @return 
+     */
+    public String[] getInputs() {
+        
+        // Declare the array to have the number of elements you intend to get 
+        // from the user.
+        String[] inputs = new String[1];
+        
+        inputs[0] = getUserInput("Pease enter the precent you wish to pay as a hole number.");
+        
+        // Repeat for each input you need, putting it into its proper slot in the array.
+        
+        return inputs;
+    }
+    
+    
+    /**
+     * Perform the action indicated by the user's input.
+     * @param inputs
+     * @return true if the view should repeat itself, and false if the view
+     * should exit and return to the previous view.
+     */
+    public boolean doAction(String[] inputs){
+        
+        double payTheTithesAndOfferings = ManageCropsControl.payTheTithesAndOfferings(0,0);
+                System.out.println(payTheTithesAndOfferings);
+        return true;
+    }
+    
+    
+    /**
+     * Control this view's display/prompt/action loop until the user
+     * chooses and action that causes this view to close.
+     */
+    public void displayView(){
+        
+        boolean keepGoing = true;
+        
+        while(keepGoing == true){
+            
+            System.out.println(message);
+            String[] inputs = getInputs();
+            keepGoing = doAction(inputs);
+        }
+    }
+    
+    
+    // Define your action handlers here. These are the methods that your doAction()
+    // method will call based on the user's input. We don't want to do a lot of 
+    // complex game stuff in our doAction() method. It will get messy very quickly.
+    
+    
+    private boolean someActionHandler(){
+        // Define whatever code you need here to accomplish the action.
+        // You can make this a void method if you want. Whatever you need 
+        // here, you are free to do.
+        //
+        // Generally, though, this is where you will call into your Control
+        // classes to do the work of the application.
+        
+        return true;
+    }
+
+}
